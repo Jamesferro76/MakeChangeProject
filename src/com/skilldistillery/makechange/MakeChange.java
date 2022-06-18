9c24e448d05f5a55dcb3685fe6d366c4a0ce2ea6
Jamesferro76 <jamesferromusic@gmail.com>
2022-06-18 10:34:29 -0600

    Another transaction?

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -8,28 +8,36 @@ public class MakeChange {
 
 		Scanner sc = new Scanner(System.in);
 		double tendered, itemCost;
+		int x = 0;
 
-		System.out.println("What is the price of the item?");
-		itemCost = sc.nextDouble();
+		while (x == 0) {
 
-		System.out.println("How much money was tendered by the customer?");
-		tendered = sc.nextDouble();
+			System.out.println("What is the price of the item?");
+			itemCost = sc.nextDouble();
 
-		String displayMessage = tendered - itemCost == 0 ? "Exact" : tendered - itemCost > 0 ? "Change" : "NotEnough";
+			System.out.println("How much money was tendered by the customer?");
+			tendered = sc.nextDouble();
 
-		switch (displayMessage) {
-		case "Exact":
-			System.out.println("Your item was paid for and there is no change. Have a great day!");
-			break;
+			String displayMessage = tendered - itemCost == 0 ? "Exact"
+					: tendered - itemCost > 0 ? "Change" : "NotEnough";
 
-		case "Change":
-			changeForCustomer(tendered, itemCost);
-			// work on getting this to work with the method
-			break;
+			switch (displayMessage) {
+			case "Exact":
+				System.out.println("Your item was paid for and there is no change. Have a great day!");
+				x = goAgain(sc, x);
 
-		case "NotEnough":
-			System.out.println("You do not have enough money to purchase this item.");
-			break;
+				break;
+
+			case "Change":
+				changeForCustomer(tendered, itemCost);
+				x = goAgain(sc, x);
+				break;
+
+			case "NotEnough":
+				System.out.println("You do not have enough money to purchase this item.");
+				x = goAgain(sc, x);
+				break;
+			}
 		}
 		sc.close();
 
@@ -41,13 +49,12 @@ public class MakeChange {
 		bill20 = bill10 = bill5 = bill1 = quarter = dime = nickle = penny = 0;
 
 		double originalTendered = tendered;
-		double originalItemCost= itemCost;
-		
+		double originalItemCost = itemCost;
 
 //		To avoid double inaccuracy tender and itemCost will be multiplied by 100
-		tendered*=100;
-		itemCost*=100;
-		
+		tendered *= 100;
+		itemCost *= 100;
+
 		while (tendered >= itemCost) {
 			double changeLeftOver = (tendered) - (itemCost);
 
@@ -80,9 +87,9 @@ public class MakeChange {
 //						+ " Twenty dollar bills, " + bill10 + " Ten dollar bills, " + bill5 + " Five dollar bills, "
 //						+ bill1 + " One dollar bills, " + quarter + " quarters, " + dime + " dimes, " + nickle
 //						+ " nickles, " + penny + " pennies");
-				
-				System.out.print("Amount: "+ originalItemCost + ", Tendered: " + originalTendered + ", Change: ");
-				
+
+				System.out.print("Amount: " + originalItemCost + ", Tendered: " + originalTendered + ", Change: ");
+
 				printOutChange(bill20, bill10, bill5, bill1, quarter, dime, nickle, penny);
 				break;
 
@@ -91,51 +98,82 @@ public class MakeChange {
 		}
 	}
 
-	public static void printOutChange(int bill20, int bill10, int bill5, int bill1, int quarter, int dime, int nickle,int penny) {
-		
-		if(bill20>=2) {
+	public static void printOutChange(int bill20, int bill10, int bill5, int bill1, int quarter, int dime, int nickle,
+			int penny) {
+
+		if (bill20 >= 2) {
 			System.out.print(bill20 + " Twenty dollar bills, ");
-		} else if(bill20==1) {
+		} else if (bill20 == 1) {
 			System.out.print(bill20 + " Twenty dollar bill, ");
 		}
-		if(bill10>=2) {
+		if (bill10 >= 2) {
 			System.out.print(bill10 + " Ten dollar bills, ");
-		} else if(bill10==1){
+		} else if (bill10 == 1) {
 			System.out.print(bill10 + " Ten dollar bill, ");
 		}
-		if(bill5>=2) {
+		if (bill5 >= 2) {
 			System.out.print(bill5 + " Five dollar bills, ");
-		} else if(bill5==1) {
-			System.out.print(bill5+ " five dollar bill, ");
+		} else if (bill5 == 1) {
+			System.out.print(bill5 + " five dollar bill, ");
 		}
-		if(bill1>=2) {
+		if (bill1 >= 2) {
 			System.out.print(bill1 + " One dollar bills, ");
-		}else if(bill1==1) {
+		} else if (bill1 == 1) {
 			System.out.print(bill1 + " One dollar bill, ");
 		}
-		if(quarter>=2) {
+		if (quarter >= 2) {
 			System.out.print(quarter + " Quarters, ");
-		}else if(quarter==1) {
+		} else if (quarter == 1) {
 			System.out.print(quarter + " Quarter, ");
 		}
-		if(dime>=2) {
+		if (dime >= 2) {
 			System.out.print(dime + " Dimes, ");
-		}else if(dime==1) {
+		} else if (dime == 1) {
 			System.out.print(dime + " Dime, ");
 		}
-		if(nickle>=2) {
+		if (nickle >= 2) {
 			System.out.print(nickle + " Nickles, ");
-		}else if(nickle==1) {
+		} else if (nickle == 1) {
 			System.out.print(nickle + " Nickle, ");
 		}
-		if(penny>=2) {
+		if (penny >= 2) {
 			System.out.print(penny + " Pennies.");
-		}else if(penny==1) {
+		} else if (penny == 1) {
 			System.out.print(penny + " Penny.");
-		}else {
+		} else {
 			System.out.print(".");
 		}
-		
+
+	}
+
+	public static int goAgain(Scanner sc, int x) {
+		int y = 0;
+
+		while (y == 0) {
+			
+			System.out.println("Would you like to do another transaction?");
+			String reply = sc.next();
+			
+			switch (reply) {
+			case "Yes":
+			case "yes":
+			case "y":
+			case "Y":
+				y = 1;
+				break;
+			case "No":
+			case "no":
+			case "N":
+			case "n":
+				x = 1;
+				y = 1;
+				break;
+			default:
+				System.out.println("That was not a readable response.");
+				break;
+			}
+		}
+		return x;
 	}
 
 }
